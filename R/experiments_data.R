ac4e068c3402b1455ba7b2c7c32ad37c789a4aeb
Caitlin Winkler <winklercaitlin@gmail.com>
2024-02-02 06:06:35 -0700

    Try chatGPT Windows fix suggestion

--- R/experiments_data.R
+++ R/experiments_data.R
@@ -294,6 +294,7 @@ pluto_get_seurat_object <- function(experiment_id, seurat_type, silent = FALSE){
 
   temp_filename <- tempfile()
   pluto_download_seurat_object(experiment_id, seurat_type, dest_filename = temp_filename)
+  closeAllConnections()
   final_obj <- readRDS(temp_filename)
   file.remove(temp_filename)
 
